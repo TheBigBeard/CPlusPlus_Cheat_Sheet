3e8205de63b6c80c787b4b0a6ca8980cb0851123
None <None>
2020-11-29 17:37:20 -0800

    Started working on project.

---
+++ Main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Arthur Rumrill on 11/29/20.
+//
+
+#include "Main.h"
---
+++ Main.h
@@ -0,0 +1,14 @@
+//
+// Created by Arthur Rumrill on 11/29/20.
+//
+
+#ifndef CPLUSPLUS_CHEAT_SHEET_MAIN_H
+#define CPLUSPLUS_CHEAT_SHEET_MAIN_H
+
+
+class Main {
+
+};
+
+
+#endif //CPLUSPLUS_CHEAT_SHEET_MAIN_H
